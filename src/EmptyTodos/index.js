9f538d02d58e6746efcad848d09b9dde124c5a1e
Diego Torres <dto1902@gmail.com>
2022-02-18 15:34:08 -0500

    revision

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -1,5 +1,15 @@
+import '../TodosLoading/TodosLoading.css';
+import { GiCheckMark } from 'react-icons/gi';
+import { MdDelete } from 'react-icons/md'
+
 function EmptyTodos() {
-    return <p>crea tu primer todo</p>
+    return (
+        <div className='LoadingTodo-container'>
+            <span className='LoadingTodo-completeIcon'><GiCheckMark /></span>
+            <p className='LoadingTodo-text'>Create your first task</p>
+            <span className='LoadingTodo-deleteIcon'><MdDelete /></span>
+        </div>
+        )
 }
 
 export { EmptyTodos }
